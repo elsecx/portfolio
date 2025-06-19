81ec174fa567868ab250652469a2f2e160b6cc83
elsecx <alwafielgin@gmail.com>
2025-06-19 15:17:07 +0700

    set: HeroSection

--- src/components/views/HomeView/HomeView.tsx
+++ src/components/views/HomeView/HomeView.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import MainLayout from "@/components/layouts/MainLayout";
 import ScrollHint from "@/components/commons/ScrollHint";
+import HeroSection from "./sections/HeroSection";
 
 const HomeView = () => {
     return (
         <MainLayout title="Home">
-            <div className="relative h-[200vh]"></div>
+            <HeroSection />
             <ScrollHint />
         </MainLayout>
     );
---
+++ src/components/views/HomeView/sections/HeroSection.tsx
@@ -0,0 +1,7 @@
+import { motion } from "framer-motion";
+
+const HeroSection = () => {
+    return <h1>Oke</h1>;
+};
+
+export default HeroSection;
