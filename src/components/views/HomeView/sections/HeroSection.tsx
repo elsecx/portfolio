ee0b2fb57b77760d371123288f84f79ee572eb83
elsecx <alwafielgin@gmail.com>
2025-06-25 09:42:49 +0700

    add: background image in HeroSection

--- src/components/views/HomeView/sections/HeroSection.tsx
+++ src/components/views/HomeView/sections/HeroSection.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { AnimatePresence, motion, useTransform } from "framer-motion";
 import useScrollTrack from "@/hooks/useScrollTrack";
 import { useScrambleText } from "@/hooks/useScrambleText";
+import heroBg from "@/assets/images/hero.jpg";
 
 const HeroSection = () => {
     const { ref: heroRef, scrollYProgress } = useScrollTrack({
@@ -10,11 +11,28 @@ const HeroSection = () => {
 
     const textFrom = "Let Me Introduce";
     const textTo = "I'm Elgin Al-wafi";
-    const mappedProgress = useTransform(scrollYProgress, [0, 0.3], [0, 1]);
-    const scrambledText = useScrambleText(mappedProgress, textFrom, textTo);
+    const scrambledText = useScrambleText(scrollYProgress, textFrom, textTo);
+
+    const scale = useTransform(scrollYProgress, [0, 1], ["1", "3"]);
 
     return (
-        <section ref={heroRef} className="relative h-[400vh]">
+        <section ref={heroRef} className="relative h-[200vh]">
+            {/* Background */}
+            <motion.div
+                style={{ scale }}
+                className="absolute inset-0 z-0 h-screen w-full bg-cover bg-center"
+            >
+                <div
+                    className="h-full w-full"
+                    style={{
+                        backgroundImage: `url(${heroBg.src})`,
+                        backgroundSize: "cover",
+                        backgroundPosition: "center",
+                    }}
+                />
+            </motion.div>
+
+            {/* Text Scramble */}
             <div className="sticky top-16 flex h-[calc(100vh-6rem)] flex-col items-center justify-center">
                 <h1 className="text-center text-5xl font-bold md:text-8xl">{scrambledText}</h1>
                 <AnimatePresence>
