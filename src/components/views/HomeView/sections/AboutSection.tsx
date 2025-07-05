90d3aa8db7f35d0b92fc525c4ca7b4c069e10775
elsecx <alwafielgin@gmail.com>
2025-07-05 18:13:06 +0700

    add: prepare AboutSection in HomeView

---
+++ src/components/views/HomeView/sections/AboutSection.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AboutSection = () => {
+    return (
+        <div className="h-[200vh]">
+            <h1>About</h1>
+        </div>
+    );
+};
+
+export default AboutSection;
