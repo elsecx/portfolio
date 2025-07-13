583c2d4268f2904bec1b05927f7f40ae07c6437e
elsecx <alwafielgin@gmail.com>
2025-07-14 03:21:23 +0700

    create: text clip style diagonal stipes

--- src/components/views/HomeView/sections/AboutSection.tsx
+++ src/components/views/HomeView/sections/AboutSection.tsx
@@ -16,18 +16,7 @@ const AboutSection = () => {
     return (
         <section ref={aboutRef} id="about-section" className="relative h-[400vh]">
             <div className="wrapper">
-                <motion.div
-                    style={blockquoteStyle}
-                    className="flex h-screen items-center justify-center px-4 pt-4 md:mx-auto md:w-3/5"
-                >
-                    <h1 className="text-center font-young-serif text-base leading-6 tracking-widest md:text-3xl">
-                        With vocational studies in{" "}
-                        <mark className="rounded-sm bg-emerald-400 ps-1">Software Engineering</mark>{" "}
-                        complete at eighteen, I craft polished websites and refined interfaces with{" "}
-                        <span className="underline">precision</span> and{" "}
-                        <span className="underline">purpose</span>.
-                    </h1>
-                </motion.div>
+                <h1 className="text-stripes text-8xl">About Me</h1>
             </div>
         </section>
     );
